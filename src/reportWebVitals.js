f0829cf4899bd7868f458e872d132baeeafe6c46
Richard Colaianni <44618480+richardcola@users.noreply.github.com>
2023-11-04 15:48:13 -0400

    Add files via upload

---
+++ src/reportWebVitals.js
@@ -0,0 +1,14 @@
+//43.1 React Router Intro, Springboard
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
